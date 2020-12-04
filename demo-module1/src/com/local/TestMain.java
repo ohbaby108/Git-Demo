8a8698813f2ce1ba35d35dcf8f72a159a43ede67
${ohbaby108} <${1084171538@qq.com}>
2020-12-04 18:35:53 +0800

    测试回退

--- demo-module1/src/com/local/TestMain.java
+++ demo-module1/src/com/local/TestMain.java
@@ -3,5 +3,6 @@ package com.local;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("test Change");
     }
 }
